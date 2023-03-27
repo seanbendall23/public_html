fb63d7e9787564dd68f9e52dca52d876906b83a3
Sean Bendall <seanbendall@Seans-MacBook-Pro.local>
2023-03-27 19:47:55 +0100

    password changes

--- add_movie.php
+++ add_movie.php
@@ -15,16 +15,10 @@ if (isset($_POST['ActorName']))
 {
 	$actorName = $_POST['ActorName'];
 }
-$pass_file = fopen('/PASSWORD.txt', "r") or die("Unable to open file!");
-echo fread($pass_file, filesize("/PASSWORD.txt"));
-fclose($pass_file);
-echo "Hello World";
 
-
-/*
 $db_host = 'mysql.cs.nott.ac.uk';
 $db_user = 'psysb16-COMP1004';
-$db_pass = '';
+$db_pass = '1234567';
 $db_name = 'psysb16-COMP1004';
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error)
@@ -34,5 +28,5 @@ if ($conn->connect_error)
 
 $query = "INSERT INTO Movie (mvGenre, mvPrice, mvName, actID) VALUES ($movieGenre, moviePrice, movieName, 1)"
 $result = $conn->query($query)
-*/
+
 ?>
