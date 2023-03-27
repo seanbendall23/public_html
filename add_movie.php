c56d63b31604b336866d68215b6a8e07b25d4914
Sean Bendall <seanbendall@Seans-MacBook-Pro.local>
2023-03-27 14:34:18 +0100

    add_movie.php changes

--- add_movie.php
+++ add_movie.php
@@ -1,7 +1,22 @@
 <?php
 if (isset($_POST['MovieName']))
 {
-		$movieName = $_POST['MovieName'];
-		echo $movieName;
+	$movieName = $_POST['MovieName'];
+	echo $movieName;
+}
+if (isset($_POST['MovieGenre']))
+{
+	$movieGenre = $_POST['MovieGenre'];
+	echo $movieGenre;
+}
+if (isset($_POST['MoviePrice']))
+{
+	$moviePrice = $_POST['MoviePrice'];
+	echo $moviePrice;
+}
+if (isset($_POST['ActorName']))
+{
+	$actorName = $_POST['ActorName'];
+	echo $actorName;
 }
 ?>
