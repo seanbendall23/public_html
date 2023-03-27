9840c3a4cad0ed546408cbc3eb9d9144590769b4
Sean Bendall <seanbendall@Seans-MacBook-Pro.local>
2023-03-27 17:08:55 +0100

    sql functionality

--- add_movie.php
+++ add_movie.php
@@ -15,4 +15,18 @@ if (isset($_POST['ActorName']))
 {
 	$actorName = $_POST['ActorName'];
 }
+
+$db_host = 'mysql.cs.nott.ac.uk';
+$db_user = 'psysb16';
+$db_pass = 'Password';
+$db_name = 'psysb16-COMP1004';
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error)
+{
+	die($conn->connect_error);
+}
+
+$query = "INSERT INTO Movie (mvGenre, mvPrice, mvName, actID) VALUES ($movieGenre, moviePrice, movieName, 1)"
+$result = $conn->query($query)
+
 ?>
