abca7dcd1bee5e1674bbeb94b8b41a86e8109800
Sean Bendall <seanbendall@Seans-MacBook-Pro.local>
2023-03-29 16:34:54 +0100

    formatting

--- search_movie.php
+++ search_movie.php
@@ -1,42 +1,52 @@
-<?php
-if (isset($_POST['MovieName']))
-{
-	$movieName = $_POST['MovieName'];
-}
 
-$db_host = 'mysql.cs.nott.ac.uk';
-$db_user = 'psysb16_COMP1004';
-$db_pass = '1234567';
-$db_name = 'psysb16_COMP1004';
+<html>
+	<head>
+		<title>Database Querying</title>
+	</head>
+	<body>
+		<h1>ADM - Actor Database Manager</h1>
+		<?php
+			if (isset($_POST['MovieName']))
+			{
+				$movieName = $_POST['MovieName'];
+			}
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-if ($conn->connect_error)
-{
-	die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection made successfully <br>";
+			$db_host = 'mysql.cs.nott.ac.uk';
+			$db_user = 'psysb16_COMP1004';
+			$db_pass = '1234567';
+			$db_name = 'psysb16_COMP1004';
 
-$ID = 0;
-$query = "SELECT mvID, actID, mvGenre, mvPrice FROM Movie WHERE mvName = '$movieName'";
-$stmt = $conn->prepare($query);
-$stmt->execute();
-$stmt->bind_result($ID, $ActorID, $Genre, $Price);
+			$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+			if ($conn->connect_error)
+			{
+				die("Connection failed: " . $conn->connect_error);
+			}
 
-$count = 1;
+			$ID = 0;
+			$query = "SELECT mvID, actID, mvGenre, mvPrice FROM Movie WHERE mvName = '$movieName'";
+			$stmt = $conn->prepare($query);
+			$stmt->execute();
+			$stmt->bind_result($ID, $ActorID, $Genre, $Price);
 
-echo "Results: ";
+			$count = 1;
 
-while ($stmt->fetch())
-{
-	echo "<br>";
-	echo "Movie #" . htmlentities($count) . " :<br>";
-	echo "ID: " . htmlentities($ID) . "<br>";
-	echo "Genre: " . htmlentities($Genre) . "<br>";
-	echo "Price: " . htmlentities($Price) . "<br>";
-	$count = $count + 1;
-}
-if ($count == 1)
-{
-	echo "<script> alert('This movie does not exist.');</script>";
-}
-?>
+			echo "Results: ";
+
+			while ($stmt->fetch())
+			{
+				echo "<br>";
+				echo "Movie #" . htmlentities($count) . " :<br>";
+				echo "ID: " . htmlentities($ID) . "<br>";
+				echo "Genre: " . htmlentities($Genre) . "<br>";
+				echo "Price: " . htmlentities($Price) . "<br>";
+				$count = $count + 1;
+			}
+			if ($count == 1)
+			{
+				echo "<script> alert('This movie does not exist.');</script>";
+			}
+		?>
+		<br><br>
+		<button type="button" onclick="window.location.href='http://avon.cs.nott.ac.uk/~psysb16/application.html';">Return to menu.</button><br>
+	</body>
+</html>
