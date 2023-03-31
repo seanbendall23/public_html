16e39b2e27b416e3d1ed13c054d909a949eea9a5
Sean Bendall <seanbendall@Seans-MacBook-Pro.local>
2023-03-31 12:05:34 +0100

    search actor changes

--- search_actor.php
+++ search_actor.php
@@ -2,9 +2,10 @@
 <html>
 	<head>
 		<title>Database Querying</title>
+		<link rel="stylesheet" href="styles.css">
 	</head>
 	<body>
-		<h1>ADM - Actor Database Manager</h1>
+		<h1 class="h1 h1a">ADM - Actor Database Manager</h1>
 		<?php
 			if (isset($_POST['ActorName']))
 			{
@@ -30,22 +31,26 @@
 
 			$count = 1;
 
-			echo "Results: ";
+			echo "<p class='p p1'> Results: </p>";
 
 			while ($stmt->fetch())
 			{
 				echo "<br>";
-				echo "Actor #" . htmlentities($count) . " :<br>";
-				echo "ID: " . htmlentities($actorID) . "<br>";
-				echo "Name: " . htmlentities($actorName) . "<br>";
+				echo "<p class='p p1'>Actor #" . htmlentities($count) . " :<br></p>";
+				echo "<p class='p p1'>ID: " . htmlentities($actorID) . "<br></p>";
+				echo "<p class='p p1'>Name: " . htmlentities($actorName) . "<br></p>";
 				$count = $count + 1;
 			}
 			if ($count == 1)
 			{
 				echo "<script> alert('This actor does not yet exist.');</script>";
+				echo "<p class='p p1'>This actor does not yet exist.</p>"
 			}
 		?>
 		<br><br>
-		<button type="button" onclick="window.location.href='http://avon.cs.nott.ac.uk/~psysb16/application.html';">Return to menu.</button><br>
+		<div align='center'>
+
+			<button type="button" onclick="window.location.href='http://avon.cs.nott.ac.uk/~psysb16/application.html';">Return to menu.</button><br>
+		</div>
 	</body>
 </html>
