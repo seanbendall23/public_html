0d9fae80434aa8e380f239429c25afb5f0965458
Sean Bendall <seanbendall@Seans-MacBook-Pro.local>
2023-03-31 11:36:28 +0100

    changes

--- search_movie.php
+++ search_movie.php
@@ -32,7 +32,7 @@
 
 			$count = 1;
 
-			echo "<p class='p p1'> Results: </p>";
+			echo "<p class='p p1'> Results for search $movieName: </p>";
 
 			while ($stmt->fetch())
 			{
@@ -40,7 +40,7 @@
 				echo "<p class='p p1'> Movie #" . htmlentities($count) . " :<br></p>";
 				echo "<p class='p p1'>ID: " . htmlentities($ID) . "<br></p>";
 				echo "<p class='p p1'>Genre: " . htmlentities($Genre) . "<br></p></p>";
-				echo "<p class='p p1'>Price: " . htmlentities($Price) . "<br></p>";
+				echo "<p class='p p1'>Price: " . htmlentities($Price) . "<br><br></p>";
 				$count = $count + 1;
 			}
 			if ($count == 1)
